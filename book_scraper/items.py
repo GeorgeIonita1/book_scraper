6650e91b6b697b4dd6d5aa7158df47aed71ecb26
george <g3orrg301@gmail.com>
2025-08-08 12:28:58 +0300

    Add a version with ItemLoaders

--- book_scraper/items.py
+++ book_scraper/items.py
@@ -1,9 +1,33 @@
 import scrapy
-from itemloaders.processors import TakeFirst, MapCompose, Join
+from itemloaders.processors import TakeFirst, MapCompose
+import re
 
+def parse_stock_status(value):
+    return "Available" if "In stock" in value else "Unavailable"
+
+def parse_stock_qty(value):
+    match = re.search(r'\((\d+) available\)', value)
+
+    return int(match.group(1)) if match else 0
 
 class BookScraperItem(scrapy.Item):
+    # For some reason, this is not correctly applied?
+    default_output_processor = TakeFirst()
+
+    page_number = scrapy.Field(output_processor=TakeFirst())
     title = scrapy.Field(output_processor=TakeFirst())
-    # price = scrapy.Field(
-    #     input_processor=MapCompose(str.strip)
-    # )
+    price = scrapy.Field(
+        input_processor=MapCompose(str.strip, lambda v: v.replace('£', '')),
+        output_processor=TakeFirst()
+    )
+    stock_status = scrapy.Field(
+        input_processor=MapCompose(str.strip, parse_stock_status),
+    output_processor=TakeFirst()
+    )
+    stock_qty = scrapy.Field(
+        input_processor=MapCompose(str.strip, parse_stock_qty),
+    output_processor=TakeFirst()
+    )
+    upc = scrapy.Field(output_processor=TakeFirst())
+    category = scrapy.Field(output_processor=TakeFirst())
+    description = scrapy.Field(output_processor=TakeFirst())
--- book_scraper/spiders/books_spider_item_loader.py
+++ book_scraper/spiders/books_spider_item_loader.py
@@ -1,23 +1,51 @@
 import scrapy
 from scrapy.loader import ItemLoader
 from book_scraper.items import BookScraperItem
+import re
 
 class BooksSpider(scrapy.Spider):
     name = "books3"
     allowed_domains = ["books.toscrape.com"]
-    start_urls = ["https://books.toscrape.com/index.html"]
+    start_urls = ["https://books.toscrape.com/catalogue/page-3.html"]
 
     def parse(self, response):
         book_links = response.xpath("//article[contains(@class, 'product_pod')]//a/@href")
+        current_page_number = self.extract_current_page_number(response)
 
         yield from response.follow_all(
             book_links,
-            self.parse_book
+            self.parse_book,
+            meta={"page_number": current_page_number}
         )
+
+        NEXT_PAGE = "//ul[contains(@class, 'pager')]/li[contains(@class, 'next')]//a/@href"
+        next_page_url = self.extract_xpath_safely(response, NEXT_PAGE)
+
+        if next_page_url and current_page_number < 3:
+            yield response.follow(next_page_url, self.parse)
 
     def parse_book(self, response):
         l = ItemLoader(item=BookScraperItem(), response=response)
 
+        l.add_value("page_number", response.meta.get("page_number"))
         l.add_xpath("title", "//h1/text()")
+        l.add_xpath("price", "//div[contains(@class, 'product_main')]//p[contains(@class, 'price_color')]/text()")
+        # Because the stock <p> element contains an <i>, the returned value when using /text() will be a new line first, and then the rest
+        # So turns out its a good practice to let the processor handle the HTML
+        l.add_xpath("stock_status", "//div[contains(@class, 'product_main')]//p[contains(@class, 'availability')]")
+        l.add_xpath("stock_qty", "//div[contains(@class, 'product_main')]//p[contains(@class, 'availability')]")
+        l.add_xpath("upc", "//th[text()='UPC']/following-sibling::td/text()")
+        l.add_xpath("category", "//ul[contains(@class, 'breadcrumb')]/li[last()-1]//a/text()")
+        l.add_xpath("description", "//div[@id='product_description']/following-sibling::p/text()")
 
         return l.load_item()
+    
+    def extract_current_page_number(self, response):
+        CURRENT_PAGE = "//ul[contains(@class, 'pager')]/li[contains(@class, 'current')]/text()"
+        pagination_text = self.extract_xpath_safely(response, CURRENT_PAGE)
+        match = re.search(r'Page (\d+) of', pagination_text)
+
+        return int(match.group(1)) if match else 1
+    
+    def extract_xpath_safely(self, response, query):
+        return response.xpath(query).get(default="").strip()
